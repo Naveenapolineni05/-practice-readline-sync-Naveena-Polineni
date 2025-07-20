844d2ad4a0c2f6f472891e087e907914966d714c
Naveena Polineni <naveenapolineni05@gmail.com>
2025-07-20 17:29:14 -0500

    readline

---
+++ readline.js
@@ -0,0 +1,31 @@
+let readline = require("readline-sync");
+
+// Greet the user
+let userName = readlineSync.question("What is your name? ");
+console.log("\nWelcome to the quiz, ${userName}!\nLet's get started!\n");
+
+
+/* Question 1
+let answer1 = readline.question("1. What data type is used to store true or false values? ");
+
+Question 2
+let answer2 = readline.question("2. What is the result of '5' + 3 in JavaScript? ");
+
+Question 3
+let answer3 = readline.question("3. What keyword is used to declare a variable that can be reassigned? ");
+
+Question 4
+let answer4 = readline.question("4. What is the output of typeof null? ");
+
+Question 5 (numeric input)
+let numberInput = readline.question("5. Enter a number to double: ");
+let doubledNumber = Number(numberInput) * 2; */
+
+/*answers
+console.log("\n===== Quiz Summary =====");
+console.log(`1. Boolean question: You answered: ${answer1}`);
+console.log(`2. '5' + 3 result: You answered: ${answer2}`);
+console.log(`3. Variable keyword: You answered: ${answer3}`);
+console.log(`4. typeof null: You answered: ${answer4}`);
+console.log(`5. You entered: ${numberInput}, doubled it is: ${doubledNumber}`);
+console.log("\nThanks for participating!"); */
